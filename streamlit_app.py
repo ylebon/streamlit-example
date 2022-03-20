0dc594dc3a1243fd3d8cace109c7e806d34cd112
Yasser Lebon <yasser.lebon@gmail.com>
2022-03-20 12:03:36 -0400

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,62 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid
+from io import StringIO
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Poc!
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def upload_file():
+    uploaded_file = st.file_uploader("Envoyer votre facture du comptoir")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
 
-    points_per_turn = total_points / num_turns
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+upload_file()
+
+"**Les prix negocies avec le client**"
+
+prix_ref = pd.read_csv("./prix_negocie.csv")
+st.dataframe(prix_ref)
+
+"**Les prix achetes au comptoir**"
+
+prix_comptoir = pd.read_csv('./prix_comptoir.csv')
+st.dataframe(prix_comptoir)
+
+# AgGrid(prix_comptoir, editable=True)
+
+"**Differences de prix**"
+
+list_produit_comptoir = prix_comptoir["Produit"].tolist()
+list_produit_ref = prix_ref["Produit"].tolist()
+
+selection = list(set(list_produit_comptoir) & set(list_produit_ref))
+
+prix_comptoir_filtered = prix_comptoir[prix_comptoir['Produit'].isin(selection)]
+prix_ref_filtered = prix_ref[prix_ref['Produit'].isin(selection)]
+
+d = []
+for i in selection:
+    prix_reference = prix_ref_filtered.set_index("Produit").loc[i][0]
+    prix_comptoir = prix_comptoir_filtered.set_index("Produit").loc[i][0]
+    if prix_reference != prix_comptoir:
+        d.append({'produit': i, "negocie": prix_reference, "comptoir": prix_comptoir})
+
+st.dataframe(pd.DataFrame(d))
